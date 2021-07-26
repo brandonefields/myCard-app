2cc20341dc463ebc8e547d2adfdc625ef74c995f
BrandonFields <brandonefields@gmail.com>
2021-07-25 19:54:10 -0600

    Initial Commit

---
+++ myCard-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myCard-app
+//
+//  Created by Brandon Fields on 7/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myCard-app/myCard_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCard_appApp.swift
+//  myCard-app
+//
+//  Created by Brandon Fields on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct myCard_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
